58975c3b2d043d40bcb65bfe7a19e6404ae9e142
ameetmadan-cleoag <ameet.madan@cleoag.ch>
2024-08-29 11:09:30 +0200

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QRCodeModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-qr-code-generator';
+    qrData: string;
+    size: number;
+    level: string;
+
+    constructor() {
+      this.qrData = 'https://www.google.com';
+      this.size = 600;
+      this.level = 'M';
+    }
+
+    @Input() data!: string;
+    @Input() qrCodeSize!: number;
 }
